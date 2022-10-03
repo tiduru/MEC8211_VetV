b3f65f97e32d1e946d1eab120bc3ba324976bf21
EduardsBlandin <84138306+EduardsBlandin@users.noreply.github.com>
2022-10-02 21:12:34 -0400

    Add files via upload
    Résous le problème tel qu'il est posé à priori, mais ne converge pas. À revoir

---
+++ Devoir1/Devoir1_MEC8211.m
@@ -0,0 +1,51 @@
+%% Clear l'espace de travail
+clc; clear all; close all;
+
+%%
+%Déclaration des constantes
+k=4*10^-9;Deff=10^(-10);S=10^-8;Ce=10;
+
+%Création du maillage en espace et en temps
+%En espace
+r_A=0;r_B=0.5;
+NELM_espace=5;
+dr=(r_B-r_A)/(NELM_espace-1);
+
+%En temps
+T=5;
+NELM_temps=10;
+dt=T/NELM_temps;
+
+%Création des variables à indexer
+syms i; 
+syms r(i); r(i)=(i-1)*dr;
+syms A(i); A(i)=r*dt*Deff;
+syms B(i); B(i)=r*dr^2;
+syms F(i);F(i)=-S*dt*r*dr^2;
+E=Deff*dt*dr;
+
+%Initialisation des matrices permettant la résolution du système
+%matricielle linéaire
+A2=zeros(NELM_espace,NELM_espace);
+A2(NELM_espace,NELM_espace)=1;
+Solution=zeros(NELM_espace,T/dt); Solution(NELM_espace,:)=Ce;
+A2(1,1:2)=[1 -1];
+syms K(i) [1 3]; K=[-A(i) B(i)+E+2*A(i) -E-A(i)];
+for j=2:NELM_espace-1
+    Solution(j,2)=F(j)+r(j)*dr^2*Solution(j,1);
+    A2(j,j-1:j+1)=subs(K,i,j);
+end
+
+%Résolution dans le temps du problème par différence finis
+for j=2:T/dt-1
+Solution(:,j)=A2\Solution(:,j-1);
+Solution(2:NELM_espace-1,j+1)=F(2:NELM_espace-1)+r(2:NELM_espace-1)*dr^2*Solution(2:NELM_espace-1,j);
+end
+Solution(:,T/dt)=A2\Solution(:,T/dt-1);
+
+%% Plot de la solution stationnaire
+syms Sol_stationnaire(i);
+Sol_stationnaire(i)=S/(4*Deff)*r_B^2*(r(i)^2/r_B^2-1)+Ce;
+
+figure (1);
+plot(subs(r,i,2:NELM_espace),subs(Sol_stationnaire,i,2:NELM_espace));
