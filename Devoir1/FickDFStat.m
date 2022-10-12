798f9c677bc93dfaa881da1779aa73af198fbb3a
jdesfos <101130220+jdesfos@users.noreply.github.com>
2022-10-12 13:32:16 -0400

    Add files via upload

---
+++ Devoir1/FickDFStat.m
@@ -0,0 +1,109 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% École Polytechnique de Montréal
+% MEC8211 A2022 Devoir 1
+%
+% Eduards Blandin
+% Jacques Desfossés
+% Timothée Duruisseau
+%
+% Cette fonction résout l'équation différentielle elliptique représentant
+% la 2e loi de Frick exprimée en coordonnées cylindriques lorsque le régime
+% stationnaire est atteint pour un terme source constant. La concentration
+% C(r,t) est calculée pour:
+%
+%   - Un pilier de béton de rayon R submergé dans une eau saline
+%   - Un pilier (cylindre) infiniment haut
+%   - Une concentration Ce=10 mol/m^3 constante à la surface du pilier
+%     correspondant à une condition frontière de Dirichlet
+%   - Un flux nul en r=0 correspondant à une condition frontière de Neumann
+%   - Une concentration initiale nulle dans le pilier C(r<R,0) = 0
+%
+%  
+% Variables
+% ---------
+%   entrée : Ntot   - Nombre de noeuds, Entier >= 3
+%            schema - Schéma de différentiation: 1 - Ordre 1
+%                                                2 - Ordre 2
+%
+%   sortie : C      - Concentrations [mol/m^3]
+%                     rangées (noeuds): Ntot
+%                     Ex: C(1) = Concentration au noeud 1.
+%                         C(3) = Concentration au noeud 3.
+%
+%   test : 50 noeuds, schéma d'ordre 2: C = FrickDF(50, 2);
+%
+% Historique
+% 11-Oct-2022 : Création
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function C = FickDFStat(Ntot, schema)
+
+% Validation de toutes les variables d'entrée
+valid = true;
+if(Ntot < 3 || (floor(Ntot) ~= ceil(Ntot)))
+   disp("Le nombre de noeuds Ntot doit être un entier >= 3");
+   valid = false;
+end
+if(schema ~=1 && schema ~=2)
+   disp("L'ordre du schéma de différentiation doit être 1 ou 2");
+   valid = false;
+end
+if(~valid)
+   error("Au moins un des arguments est invalide");
+end
+
+% Données du problème
+R    = 0.5;    % Rayon du pilier de béton [m]
+Ce   = 10;     % Concentration à la surface du pilier [mol/m^3]
+Deff = 10E-10; % Cefficient de diffusion effectif du sel [mol/m^3/s]
+Scst = 1E-8;   % Terme source constant pour solution analytique [mol/m^3/s]
+
+% On exprime Deff et Scst en [1/an]
+secParAn = 31536000;
+Deff = Deff * secParAn; % [mol/m^3/an]
+Scst = Scst * secParAn; % [mol/m^3/an]
+
+% Intervalles h
+h = R/(Ntot-1);
+
+% Initialisation
+C = zeros(Ntot, 1);     % Concentrations
+M = zeros(Ntot, Ntot);  % Matrice [M] à gauche du système linéaire
+V = zeros(Ntot, 1);     % Vecteur {V} à droite du système linéaire
+
+% Valeurs de [M] et {V} constantes dû aux conditions frontières
+
+% Neumann - Noeud à r=0, flux nul
+if(schema == 1)
+   M(1,1:2) = [1 -1]; % dérivée première avant (ordre 1)
+elseif(schema == 2)
+   M(1,1:3) = [-3 4 -1]; % de Gear avant (ordre 2)
+end
+V(1,1) = 0;
+
+% Dirichlet - Noeud à r=R, concentration constante Ce
+M(Ntot,Ntot) = 1;
+V(Ntot,1)    = Ce;
+
+% Système matriciel [M]{C} = {V}
+for i=2:Ntot-1
+   ri = (i-1)*h; % Position radiale du noeud 
+
+   if(schema == 1)
+      M(i,i-1) = ri;
+      M(i,i)   = -(2*ri + h);
+      M(i,i+1) = ri + h;
+   elseif(schema == 2)
+      M(i,i-1) = ri-0.5*h;
+      M(i,i)   = -2*ri;
+      M(i,i+1) = ri + 0.5*h;
+   end
+   V(i,1) = (Scst*(h^2)*ri)/Deff;
+end
+
+% Solution au temps t: [C]=[M]^-1 {V}. On laisse Matlab déterminer le
+% meilleur algorithme en utilisant l'opérateur '\'
+C = M\V;
+
+end
---
+++ Devoir1/FickVerif.m
@@ -0,0 +1,101 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% École Polytechnique de Montréal
+% MEC8211 A2022 Devoir 1
+%
+% Eduards Blandin
+% Jacques Desfossés
+% Timothée Duruisseau
+%
+% Cet script permet la vérification de la fonction FrickDF qui calcule 
+% l'évolution de la concentration de sel à l'intérieur d'un pilier de
+% béton à l'aide des différences finies. La vérification sera effectuée par
+% rapport à la solution analytique stationnaire lorsque le terme source est
+% constant.
+%
+% Historique
+% 03-Oct-2022 : Création
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc;
+clear all;
+
+% Paramètres de la solution par différences finies
+Nmin   = 3;    % Nombre de noeuds minimal
+Nmax   = 10;   % Nombre de noeuds maximal
+R      = 1;    % Rayon du pilier [m]
+dt     = 1;    % Pas de temps [an]
+Ndt    = 1000; % Nombre de pas de temps
+tsMeth = 0;    % Méthode "constante" pour le terme source
+
+% Calcul des erreurs L1, L2 et Linf pour un nombre de noeuds dans le
+% maillage: Nmin <= Ntot <= Nmax
+Niter = Nmax - Nmin + 1;
+h  = zeros(Niter, 1);
+L1 = zeros(Niter, 2);
+L2 = zeros(Niter, 2);
+Linf = zeros(Niter, 2);
+for i=1:Niter
+   Ntot = Nmin + (i-1);  % Nombre de noeuds pour cette itération
+   h(i) = R/(Ntot-1);    % Intervalle pour cette itération [m]
+   [CO1, t1] = FickDF(Ntot, dt, Ndt, 1, tsMeth); % Concentrations, schéma O(1)
+   [CO2, t2] = FickDF(Ntot, dt, Ndt, 2, tsMeth); % Concentrations, schéma O(2)
+   Cana = FickAnaStat(Ntot); % Concentrations analytiques [mol/m^3]
+
+   for j=1:Ntot
+      % Ordre 1
+      diffabs1 = abs(CO1(Ndt+1,j) - Cana(j));
+      L1(i,1) = L1(i,1) + diffabs1;
+      L2(i,1) = L2(i,1) + diffabs1^2;
+      if(diffabs1 > Linf(i,1))
+         Linf(i,1) = diffabs1;
+      end
+      
+      % Ordre 2
+      diffabs2 = abs(CO2(Ndt+1,j) - Cana(j));    
+      L1(i,2) = L1(i,2) + diffabs2;
+      L2(i,2) = L2(i,2) + diffabs2^2;
+      if(diffabs2 > Linf(i,2))
+         Linf(i,2) = diffabs2;
+      end
+   end
+
+   % Ordre 1
+   L1(i,1) = L1(i,1)/Ntot;
+   L2(i,1) = sqrt(L2(i,1)/Ntot);
+   
+   % Ordre 2
+   L1(i,2) = L1(i,2)/Ntot;
+   L2(i,2) = sqrt(L2(i,2)/Ntot);
+end
+
+% Ordre de précision observé
+a = 1; % 1er point pour déterminer la pente
+b = 3; % 2e  point pour déterminer la pente
+dh = log(h(a)/h(b));
+
+p_hat_L1   = log(L1(a,1)/L1(b,1))/dh;
+p_hat_L2   = log(L2(a,1)/L2(b,1))/dh;
+p_hat_Linf = log(Linf(a,1)/Linf(b,1))/dh;
+disp(sprintf("p_hat O(1): L1=%f, L2=%f, Linf=%f", p_hat_L1, p_hat_L2, p_hat_Linf))
+
+p_hat_L1   = log(L1(a,2)/L1(b,2))/dh;
+p_hat_L2   = log(L2(a,2)/L2(b,2))/dh;
+p_hat_Linf = log(Linf(a,2)/Linf(b,2))/dh;
+disp(sprintf("p_hat O(2): L1=%f, L2=%f, Linf=%f", p_hat_L1, p_hat_L2, p_hat_Linf))
+
+
+% Création des graphes
+figure
+subplot(2,1,1);
+loglog(h, L1(:,1), h, L2(:,1), h, Linf(:,1));
+title('Erreurs pour un schéma de différenciation O(1)');
+xlabel('h');
+ylabel('Erreur');
+legend('L_{l}', 'L_{2}', 'L_{\infty}');
+
+subplot(2,1,2);
+loglog(h, L1(:,2), h, L2(:,2), h, Linf(:,2));
+title('Erreurs pour un schéma de différenciation O(2)');
+xlabel('h');
+ylabel('Erreur');
+legend('L_{l}', 'L_{2}', 'L_{\infty}');
