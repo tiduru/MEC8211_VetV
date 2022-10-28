0f436f270864de6bce54cff3b01e0bf61952499b
tiduru <76910886+tiduru@users.noreply.github.com>
2022-10-28 14:17:00 -0400

    MaJ code AnalyseConvergence + data

--- Devoir2/AnalyseConvergence.m
+++ Devoir2/AnalyseConvergence.m
@@ -2,33 +2,31 @@
 % Obtention de données pour
 % analyse de convergence
 %---------------------------
-clear all; clc;
+clear all; clc; close all;
 %% Settings
 N = [3,5,9,17,33];
-dt = [0.001,0.01,0.1,0.25,1];
-dt_text = ["0001","001","01","025","1"];
+dt = [0.001,0.01,0.1,1];
+dt_text = ["0001","001","01","1"];
 tf = [1,10,25];
 
 %% Data from COMSOL
 DataDirectory = "./DataAnalyseConvergence/";
-VS = "Variation spatial/";
-VT = ["Variation temporel/N5/","Variation temporel/N17/"];
+VS = "Variation spatiale/";
+VT = "Variation temporelle/";
 
 DataS = {}; % {N, t}(2)
 
 for i = 1:length(N)
     for j = 1:length(tf)
-        DataS{i,j} = load(DataDirectory+VS+"N"+string(N(i))+"t"+string(tf(j))+"dt"+dt_text(4)+".txt");
+        DataS{i,j} = load(DataDirectory+VS+"N"+string(N(i))+"t"+string(tf(j))+"dt"+dt_text(3)+".txt");
     end
 end
 
-DataT5 = {}; % {t, dt}(2)
-DataT17 = {};
+DataT = {}; % {t, dt}(2)
 
 for i = 1:length(tf)
     for j = 1:length(dt)
-        DataT5{i,j} = load(DataDirectory+VT(1)+"N"+string(N(2))+"t"+string(tf(i))+"dt"+dt_text(j)+".txt");
-        DataT17{i,j} = load(DataDirectory+VT(2)+"N"+string(N(4))+"t"+string(tf(i))+"dt"+dt_text(j)+".txt");
+        DataT{i,j} = load(DataDirectory+VT+"N"+string(N(end))+"t"+string(tf(i))+"dt"+dt_text(j)+".txt");
     end
 end
 
@@ -36,16 +34,14 @@ end
 SimS = {};
 for i = 1:length(N)
     for j = 1:length(tf)
-        [SimS{i,j}(:,2),SimS{i,j}(:,1)] = FickDF(N(i),dt(4),tf(j)/dt(4),2,1);
+        [SimS{i,j}(:,2),SimS{i,j}(:,1)] = FickDF(N(i),dt(3),tf(j)/dt(3),2,1);
     end
 end
 
-SimT5 = {};
-SimT17 = {};
+SimT = {};
 for i = 1:length(tf)
     for j = 1:length(dt)
-        [SimT5{i,j}(:,2) , SimT5{i,j}(:,1)] = FickDF(N(2),dt(j),tf(i)/dt(j),2,1);
-        [SimT17{i,j}(:,2) , SimT17{i,j}(:,1)] = FickDF(N(4),dt(j),tf(i)/dt(j),2,1);
+        [SimT{i,j}(:,2) , SimT{i,j}(:,1)] = FickDF(N(end),dt(j),tf(i)/dt(j),2,1);
     end
 end
 
@@ -54,13 +50,9 @@ L1S = [];
 L2S = [];
 LinfS = [];
 
-L1T5 = [];
-L2T5 = [];
-LinfT5 = [];
-
-L1T17 = [];
-L2T17 = [];
-LinfT17 = [];
+L1T = [];
+L2T = [];
+LinfT = [];
 
 for i = 1:length(N)
     for j = 1:length(tf)
@@ -82,53 +74,46 @@ end
 
 for i = 1:length(tf)
     for j = 1:length(dt)
-        L1T5(i,j) = 0;
-        L2T5(i,j) = 0;
-        LinfT5(i,j) = 0;
-        for k = 1:size(DataT5{i,j},2)
-            diffabs = abs(DataT5{i,j}(k,2) - SimT5{i,j}(k,2));
-            L1T5(i,j) = L1T5(i,j) + diffabs;
-            L2T5(i,j) = L2T5(i,j) + diffabs^2;
-            if diffabs > LinfT5(i,j)
-                LinfT5(i,j) = diffabs;
+        L1T(i,j) = 0;
+        L2T(i,j) = 0;
+        LinfT(i,j) = 0;
+        for k = 1:size(DataT{i,j},2)
+            diffabs = abs(DataT{i,j}(k,2) - SimT{i,j}(k,2));
+            L1T(i,j) = L1T(i,j) + diffabs;
+            L2T(i,j) = L2T(i,j) + diffabs^2;
+            if diffabs > LinfT(i,j)
+                LinfT(i,j) = diffabs;
             end
         end
-        L1T5(i,j) = L1T5(i,j) / N(2);
-        L2T5(i,j) = sqrt(L2T5(i,j) / N(2));
-    end
-end
-for i = 1:length(tf)
-    for j = 1:length(dt)
-        L1T17(i,j) = 0;
-        L2T17(i,j) = 0;
-        LinfT17(i,j) = 0;
-        for k = 1:size(DataT17{i,j},2)
-            diffabs = abs(DataT17{i,j}(k,2) - SimT17{i,j}(k,2));
-            L1T17(i,j) = L1T17(i,j) + diffabs;
-            L2T17(i,j) = L2T17(i,j) + diffabs^2;
-            if diffabs > LinfT17(i,j)
-                LinfT17(i,j) = diffabs;
-            end
-        end
-        L1T17(i,j) = L1T17(i,j) / N(4);
-        L2T17(i,j) = sqrt(L2T17(i,j) / N(4));
+        L1T(i,j) = L1T(i,j) / N(2);
+        L2T(i,j) = sqrt(L2T(i,j) / N(2));
     end
 end
 
 for i = 1:3
     figure(i);
     hold on
-    loglog(0.5./N, L1S(:,i));
-    loglog(0.5./N, L2S(:,i));
-    loglog(0.5./N, LinfS(:,i));
+    loglog(0.5./(N-1), L1S(:,i));
+    loglog(0.5./(N-1), L2S(:,i));
+    loglog(0.5./(N-1), LinfS(:,i));
+    title({"Erreur en fonction de la variation spatiale","t = "+string(tf(i))})
+    xlabel("h [mm]")
+    ylabel("Erreur")
+    legend({"L1","L2","L_{inf}"},"location","best")
     hold off
+    grid on
 end
 
 for i = 4:6
     figure(i);
     hold on
-    loglog(dt, L1T5(i-3,:));
-    loglog(dt, L2T5(i-3,:));
-    loglog(dt, LinfT5(i-3,:));
+    loglog(dt, L1T(i-3,:));
+    loglog(dt, L2T(i-3,:));
+    loglog(dt, LinfT(i-3,:));
+    title({"Erreur en fonction de la variation temporelle","h = "+string(0.5/32)+" m ; t = "+string(tf(i-3))})
+    xlabel("dt [s]")
+    ylabel("Erreur")
+    legend({"L1","L2","L_{inf}"},"location","best")
     hold off
+    grid on
 end
